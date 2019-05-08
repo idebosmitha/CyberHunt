967bd3b7faa2ea5693253afd927adbef20c3c48c
diptangsu <deep.dippu@gmail.com>
2019-05-08 16:24:32 +0530

    added favicon

--- cyberhunt/urls.py
+++ cyberhunt/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', teams.views.home, name='home'),
     path('team/', include('teams.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+     
