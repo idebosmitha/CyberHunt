773bb06c238bd88ea3225916e9061025d49918d2
diptangsu <deep.dippu@gmail.com>
2019-05-08 16:26:58 +0530

    added static and media urls

--- cyberhunt/urls.py
+++ cyberhunt/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 import teams.views
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
