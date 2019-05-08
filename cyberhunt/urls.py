913158f855102b47606ccbf3fa270116d3e3db80
diptangsu <deep.dippu@gmail.com>
2019-05-08 18:33:11 +0530

    modified urls

--- cyberhunt/urls.py
+++ cyberhunt/urls.py
@@ -10,4 +10,3 @@ urlpatterns = [
     path('team/', include('teams.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-     
